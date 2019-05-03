75b6f69ee8c2d373ac58e55c24e8c65aee04eeea
Ashish <ashish336b@gmail.com>
2019-05-03 17:42:08 +0545

    ok

---
+++ NumberShapes/app/src/main/java/com/example/numbershapes/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.numbershapes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    public void testNumber(View view){
+        Log.i("info","Button Pressed");
+        EditText editText = (EditText) findViewById(R.id.editText);
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
